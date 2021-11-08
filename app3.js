dd9a0ff5ddd714e2ccca3b5b7436e86598c3439d
henriryytli <henriryytli1@gmail.com>
2021-11-08 06:13:41 +0200

    Add book to book table Delete single book from book table

--- app3.js
+++ app3.js
@@ -1,35 +1,47 @@
 const form = document.querySelector("form");
 const titleinput = document.querySelector("#entertitle");
 const authorinput = document.querySelector("#enterauthor");
-const isbninput = document.querySelector("enterisbn");
+const isbninput = document.querySelector("#enterisbn");
 const mycollection = document.querySelector(".mycollection");
 const mytable = document.querySelector("table");
 
 
-form.addEventListener("submit", addthings);
+form.addEventListener("submit", addBook);
+mytable.addEventListener("click", deleteBook);
 
-function addthings(e) {
+
+function deleteBook(e) {
+    if(e.target.textContent == "X") {
+        if(confirm("delete this?")) {
+            e.target.parentElement.remove();
+        }
+    }
+}
+
+
+
+function addBook(e) {
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
     const td2 = document.createElement("td");
     const td3 = document.createElement("td");
-    const li1 = document.createElement("li");
-    const li2 = document.createElement("li");
-    const li3 = document.createElement("li");
     const title = titleinput.value;
     const author = authorinput.value;
     const isbn = isbninput.value;
-    li1.appendChild(document.createTextNode(title));
-    li2.appendChild(document.createTextNode(author));
-    li3.appendChild(document.createTextNode(isbn));
-    td1.appendChild(li1);
-    td2.appendChild(li2);
-    td3.appendChild(li3);
+    const link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.className = "secondary-content";
+    link.appendChild(document.createTextNode("X"));
+
+    td1.appendChild(document.createTextNode(title));
+    td2.appendChild(document.createTextNode(author));
+    td3.appendChild(document.createTextNode(isbn));
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
+    tr.appendChild(link);
     mytable.append(tr);
-
+    e.preventDefault();
 
 
 }
